4f01e14c960c165736fca7f9eaab8b865283f0ac
Kane McConnell <kane@makanal.eu>
2018-09-10 15:15:27 +0200

    Add Core.EmailMessage.
    This update adds Core.Message which is extended by Core.EmailMessage.
    
    Issue: https://makanal.visualstudio.com/CommonEntities/_workitems/edit/44
    Signed-off-by: Kane McConnell <kane@makanal.eu>
    CLA: f863b4f7bdee7d6997520ecaf537ddf0
    
    Related Work Items: #44

---
+++ CommonEntities/Core/EmailMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace CommonEntities.Core
+{
+    /// <summary>
+    /// An email message.
+    /// </summary>
+    [DataContract(Name = "EmailMessage", Namespace = "https://schema.org/EmailMessage")]
+    public class EmailMessage : Message
+    {
+    }
+}
---
+++ CommonEntities/MultiType/Combo/Recipient.cs
@@ -0,0 +1,76 @@
+﻿using CommonEntities.Core;
+using CommonEntities.Core.Intangible;
+using CommonEntities.Core.Intangible.StructuredValue;
+using System.Runtime.Serialization;
+
+namespace CommonEntities.MultiType.Combo
+{
+    /// <summary>
+    /// Recipient MultiType accepts Audience, ContactPoint, Organization,
+    /// or Person.
+    /// </summary>
+    /// <example>https://schema.org/recipient</example>
+    [DataContract(Name = "Recipient", Namespace = "CommonEntities.MultiType.Combo")]
+    public class Recipient
+    {
+        /// <summary>
+        /// Recipient as an Audience.
+        /// </summary>
+        [DataMember(Name = "asContactPoint")]
+        public Audience AsAudience;
+
+        /// <summary>
+        /// Recipient as an ContactPoint.
+        /// </summary>
+        [DataMember(Name = "asContactPoint")]
+        public ContactPoint AsContactPoint;
+
+        /// <summary>
+        /// Recipient as an Organization.
+        /// </summary>
+        [DataMember(Name = "asContactPoint")]
+        public Organization AsOrganization;
+
+        /// <summary>
+        /// Recipient as an Person.
+        /// </summary>
+        [DataMember(Name = "asContactPoint")]
+        public Person AsPerson;
+
+        /// <summary>
+        /// Recipient as an Audience.
+        /// </summary>
+        /// <param name="audience">Recipient as an Audience.</param>
+        public Recipient(Audience audience)
+        {
+            AsAudience = audience;
+        }
+
+        /// <summary>
+        /// Recipient as an ContactPoint.
+        /// </summary>
+        /// <param name="contactPoint">ContactPoint</param>
+        public Recipient(ContactPoint contactPoint)
+        {
+            AsContactPoint = contactPoint;
+        }
+
+        /// <summary>
+        /// Recipient as an Organization.
+        /// </summary>
+        /// <param name="organization">Recipient as an Organization.</param>
+        public Recipient(Organization organization)
+        {
+            AsOrganization = organization;
+        }
+
+        /// <summary>
+        /// Recipient as an Person.
+        /// </summary>
+        /// <param name="person">Recipient as an Person.</param>
+        public Recipient(Person person)
+        {
+            AsPerson = person;
+        }
+    }
+}
