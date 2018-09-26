5ba73c23fc24b73a0749f3066d15a97c25761c42
Kane McConnell <kane@makanal.eu>
2018-09-26 15:05:40 +0200

    Add Listify.Intangible.ItemList
    Issue: https://makanal.visualstudio.com/MakanalTech.CommonEntities/_workitems/edit/53
    Signed-off-by: Kane McConnell <kane@makanal.eu>
    CLA: f863b4f7bdee7d6997520ecaf537ddf0
    
    Related Work Items: #53

---
+++ MakanalTech.CommonEntities/Listify/Intangible/ItemList.cs
@@ -0,0 +1,23 @@
+﻿using MakanalTech.CommonEntities.MultiType.Combo;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MakanalTech.CommonEntities.Listify.Intangible
+{
+    /// <summary>
+    /// Listify Extension of Core.Intangible.ItemList.
+    /// </summary>
+    /// <remarks>
+    /// The listify extension is an extension of items not identified as lists
+    /// by the core vocabulary but are more prevelantly used as lists rather
+    /// than single elements.
+    /// </remarks>
+    class ItemList : Core.Intangible.ItemList
+    {
+        /// <summary>
+        /// Core.Intangible.ItemList.ItemListElement as a list.
+        /// </summary>
+        [DataMember(Name = "itemListElements")]
+        public List<ListItemThingOrText> ItemListElements { get; set; }
+    }
+}
